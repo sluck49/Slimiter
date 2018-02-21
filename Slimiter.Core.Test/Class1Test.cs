b9f53f2efb42d06799f922e5006f9d3395a62329
Shawn Holzworth <sholzworth@igsenergy.com>
2018-02-21 13:15:44 -0500

    added start template

---
+++ Slimiter.Core.Test/Class1Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Slimiter.Core.Test
+{
+    [TestFixture]
+    public class Class1Test
+    {
+        [Test]
+        public void When()
+        {
+            Assert.Pass();
+        }
+    }
+}
